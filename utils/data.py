ce1e2852c100c2418a3b528e650793a755f64f9d
honeysuckle <labzin.as@phystech.edu>
2024-01-14 18:27:06 +0300

    Add dataset visualization

--- utils/data.py
+++ utils/data.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset, DataLoader
 
@@ -50,3 +51,95 @@ class TradingDataset (Dataset):
         test = torch.tensor(test.transpose(1, 0), dtype=torch.float32)
 
         return train, test
+
+
+
+
+# Функция для отрисовки результатов
+def show_data (loader):
+    train, test = next(iter(loader))
+
+    plt.figure(figsize=(20, 10))
+    # Цены закрытия [3]
+    plt.plot(torch.hstack((train[0, 3, :], test[0, 0, :])), "red")
+    plt.plot(train[0, 3, :], "black")
+    # Цены открытия/макс/мин/средние
+    plt.plot(train[0, 0, :], label="Open")
+    plt.plot(train[0, 1, :], label="High") 
+    plt.plot(train[0, 2, :], label="Low") 
+    plt.plot(train[0, 5, :], label="EMA 9") 
+    plt.plot(train[0, 6, :], label="EMA 20")
+    plt.plot(train[0, 7, :], label="EMA 50")
+    plt.plot(train[0, 8, :], label="EMA 200")
+    plt.legend()
+    plt.title("Prices")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 4, :], label="Volume")
+    plt.legend()
+    plt.title("Volume normalized")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 9, :], label="Volatility 9")
+    plt.plot(train[0, 10, :], label="Volatility 20")
+    plt.plot(train[0, 11, :], label="Volatility 50")
+    plt.plot(train[0, 12, :], label="Volatility 200")
+    plt.legend()
+    plt.title("Volatility")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 13, :], label="RSI 5")
+    plt.plot(train[0, 14, :], label="RSI 9")
+    plt.plot(train[0, 15, :], label="RSI 14")
+    plt.legend()
+    plt.title("RSI")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 16, :], label="MACD line")
+    plt.plot(train[0, 17, :], label="MACD signal")
+    plt.legend()
+    plt.title("MACD")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 18, :], label="Stochastic %D 5")
+    plt.plot(train[0, 19, :], label="Stochastic %D 9")
+    plt.plot(train[0, 20, :], label="Stochastic %D 14")
+    plt.legend()
+    plt.title("Stochastic")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 21, :], label="RVI 5")
+    plt.plot(train[0, 22, :], label="RVI 9")
+    plt.plot(train[0, 23, :], label="RVI 14")
+    plt.legend()
+    plt.title("Relative Vigor Index (RVI)")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 24, :], label="Money Flow Index")
+    plt.legend()
+    plt.title("Money Flow Index")
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(train[0, 25, :], label="Awesome Oscillator")
+    plt.legend()
+    plt.title("Awesome Oscillator")
+    plt.grid()
+    plt.show()
+
+    return
